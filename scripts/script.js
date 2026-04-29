794f53a4d51b8526b898fd338ac0dd6426a52231
Thais Duarte da Silva <thais_duarte-silva@estudante.sesisenai.org.br>
2026-04-29 11:08:45 -0300

    add - home

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,30 @@
 document.getElementById("form-login").onsubmit = (e) => {
     e.preventDefault();
 
-    let email = document.getElementById("email").value;
+    let usuario = document.getElementById("usuario").value;
     let senha = document.getElementById("senha").value;
     let mensagem = document.getElementById("mensagem");
     mensagem.innerHTML = "";
-    if (!email.includes("@") || !email.includes(".")) {
-        mensagem.innerHTML = "<div class='erro'><p> Email Invalido !</p></div>";
-        return;
 
+    if (usuario.length < 4) {
+        mensagem.innerHTML = "<div class='erro'><p> Nome de usuário muito curto!</p></div>";
+        return;
     }
     if (senha.length < 4) {
-        mensagem.innerHTML = "<div class='erro'><p> Senha muito curta !</p></div>";
+        mensagem.innerHTML = "<div class='erro'><p> Senha muito curta!</p></div>";
         return;
     }
-    if (email === ""){
-        
+    if (usuario === "") {
+        mensagem.innerHTML = "<div class='erro'><p> Campo obrigatório! </p></div>";
+        return;
+
+    }
+    if (senha === "") {
+        mensagem.innerHTML = "<div class='erro'><p> Campo obrigatório! </p></div>";
+        return;
 
     }
 
-
     document.getElementById("form-login").reset();
+    window.location.href = "home.html";
 }
